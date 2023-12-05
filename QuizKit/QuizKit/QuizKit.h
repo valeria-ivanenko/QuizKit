55dad16154f009a72e9c9d41423913e7faa15a8c
valeria-ivanenko <98172492+valeria-ivanenko@users.noreply.github.com>
2023-12-05 21:46:16 +0200

    Initial files

---
+++ QuizKit/QuizKit/QuizKit.h
@@ -0,0 +1,18 @@
+//
+//  QuizKit.h
+//  QuizKit
+//
+//  Created by Valeriia Ivanenko on 05.12.2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for QuizKit.
+FOUNDATION_EXPORT double QuizKitVersionNumber;
+
+//! Project version string for QuizKit.
+FOUNDATION_EXPORT const unsigned char QuizKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <QuizKit/PublicHeader.h>
+
+
